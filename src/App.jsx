b4c3364efb369e16e1fbda581ae6c0c700fc6e03
Jose <josedelvalle012@gmail.com>
2025-10-06 12:36:49 -0300

    dist/gh-pages

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Hero from './components/Hero'
+import Menu from './components/Menu'
+import Services from './components/Services'
+import CallToAction from './components/CallToAction'
+import Testimonials from './components/Testimonials'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Hero></Hero>
+      <Menu></Menu>
+      <Services></Services>
+      <CallToAction></CallToAction>
+      <Testimonials></Testimonials>
+    </>
+  )
+}
+
+export default App
